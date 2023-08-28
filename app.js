ffc247776e76e06cf6f0ec8f7e19aef091672889
bindu0102 <143309687+bindu0102@users.noreply.github.com>
2023-08-28 21:07:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function login(){
+    let username=document.getElementById('username').value
+    let password=document.getElementById('password').value
+    console.log(username)
+    console.log(password)
+if(username=="bindu"&&password=="b23"){
+    open("instagram.html")
+}
+else{
+    alert("stupid...not a valid man")
+}
+}
